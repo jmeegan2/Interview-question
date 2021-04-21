58ff2701a7675c9746b9f1c58691702b04d1b85e
jmeegan2 <jmeegan2@owlmail.harford.edu>
2021-04-21 16:20:43 -0400

    Quick little program there was a tutorial online

--- Source.cpp
+++ Source.cpp
@@ -1,15 +1,37 @@
 #include<iostream>
 using namespace std;
 
+void findMultiples(int n)
+{
+	int a = 3; //to keep track of multiple of 3
+	int b = 5; //to keep track of multiple of 5
+	for (int i = 1; i <= n; i++)
+	{
+
+		string s = "";
+
+		//Found multiple of 3
+		if (i == a)
+		{
+			a = a + 3; //update next multiple of 3
+			s = s + "Buzz ";
+		}
+		//found multiple of 5
+		if (i == b)
+		{
+			b = b + 5; //update next multiple of 5
+			s = s + "Fizz ";
+		}
+		if (s == "")
+			cout << (i) << endl;
+		else
+			cout << (s) << endl;
+	}
+}
+
 int main()
 {
-	int number = 1;
-	
-	while (number <= 100)
-	{
-		cout << number << " ";
-		number = number + 1;
-	}
+	findMultiples(100);
 
 	return 0;
 }
